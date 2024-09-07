ea87300e9a8938202e8c474b69a8b0e6b4d76e9c
Pablo Rodriguez Albarran <pablorodriguezalbarran2000@gmail.com>
2024-09-07 13:17:10 +0200

    glide error fix

--- src/lib/carousel.js
+++ src/lib/carousel.js
@@ -3,15 +3,18 @@ import "@glidejs/glide/dist/css/glide.core.min.css";
 import "@glidejs/glide/dist/css/glide.theme.min.css";
 
 document.addEventListener("DOMContentLoaded", () => {
-  
-  const singleGlide = new Glide(".single-glide", {
-    type: "carousel",
-    perView: 1,
-    gap: 20,
-  });
+  const singleGlideElement = document.querySelector(".single-glide");
+  if (singleGlideElement) {
+    const singleGlide = new Glide(singleGlideElement, {
+      type: "carousel",
+      perView: 1,
+      gap: 20,
+    });
+    singleGlide.mount();
+  }
 
   const multipleGlides = document.querySelectorAll(".multiple-glide");
-  multipleGlides.forEach((glide, index) => {
+  multipleGlides.forEach((glide) => {
     new Glide(glide, {
       type: "carousel",
       perView: 10,
@@ -33,31 +36,34 @@ document.addEventListener("DOMContentLoaded", () => {
     }).mount();
   });
 
-  const teasersGlide = new Glide(".teasers-glide", {
-    type: "carousel",
-    perView: 5,
-    gap: 40,
-    breakpoints: {
-      1200: {
-        perView: 1,
+  const teasersGlideElement = document.querySelector(".teasers-glide");
+  if (teasersGlideElement) {
+    const teasersGlide = new Glide(teasersGlideElement, {
+      type: "carousel",
+      perView: 5,
+      gap: 40,
+      breakpoints: {
+        1200: {
+          perView: 1,
+        },
+        1024: {
+          perView: 2,
+        },
+        768: {
+          perView: 1,
+        },
       },
-      1024: {
-        perView: 2,
-      },
-      768: {
-        perView: 1,
-      },
-    }
-  });
+    });
+    teasersGlide.mount();
+  }
 
-  const castsGlide = new Glide(".casts-glide", {
-    type: "carousel",
-    perView: 3,
-    gap: 20,
-  });
-
-
-  singleGlide.mount();
-  teasersGlide.mount();
-  castsGlide.mount();
+  const castsGlideElement = document.querySelector(".casts-glide");
+  if (castsGlideElement) {
+    const castsGlide = new Glide(castsGlideElement, {
+      type: "carousel",
+      perView: 3,
+      gap: 20,
+    });
+    castsGlide.mount();
+  }
 });
