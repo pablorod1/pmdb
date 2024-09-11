fc58865f2520b8707ca66ce71a2d4bbc41006ce5
Pablo Rodríguez <pablorodriguez@lucentialab.es>
2024-09-11 14:44:51 +0200

    first complete upload

--- src/components/SerieList.jsx
+++ src/components/SerieList.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 import {
-  getPopularMovies,
-  getTopRatedMovies,
-  getTrendingMoviesDay,
-  getTrendingMoviesWeek,
-  getNowPlayingMovies,
-  getUpcomingMovies,
+  getPopularSeries,
+  getTopRatedSeries,
+  getTrendingSeriesDay,
+  getTrendingSeriesWeek,
+  getNowPlayingSeries,
+  getUpcomingSeries,
 } from "@lib/tmdb";
 
 function formatDate(date) {
@@ -73,73 +73,82 @@ function getGenreClass(genre) {
   }
 }
 
-const fetchMovies = async (type, page) => {
+const fetchSeries = async (type, page) => {
   switch (type) {
     case "top-rated":
-      return await getTopRatedMovies(page);
+      return await getTopRatedSeries(page);
     case "trendingDay":
-      return await getTrendingMoviesDay(page);
+      return await getTrendingSeriesDay(page);
     case "trendingWeek":
-      return await getTrendingMoviesWeek(page);
+      return await getTrendingSeriesWeek(page);
     case "nowPlaying":
-      return await getNowPlayingMovies(page);
+      return await getNowPlayingSeries(page);
     case "upcoming":
-      return await getUpcomingMovies(page);
+      return await getUpcomingSeries(page);
     default:
-      return await getPopularMovies(page);
+      return await getPopularSeries(page);
   }
 };
 
-export default function MovieList({ initialMovies, initialType }) {
-  const [movies, setMovies] = useState(initialMovies.movies);
+export default function SerieList({ initialSeries, initialType }) {
+  const [series, setSeries] = useState(initialSeries.series);
   const [page, setPage] = useState(2);
-  const [totalPages, setTotalPages] = useState(initialMovies.totalPages);
+  const [totalPages, setTotalPages] = useState(initialSeries.totalPages);
   const [type, setType] = useState(initialType);
 
   // Función para comprobar si una película ya existe en el array
-  const isMovieInList = (movieId) => {
-    return movies.some((movie) => movie.id === movieId);
+  const isSerieInList = (serieId) => {
+    return series.some((serie) => serie.id === serieId);
   };
 
-  const loadMoreMovies = async () => {
-    const { movies: newMovies, totalPages: newTotalPages } = await fetchMovies(
+
+  const loadMoreSeries = async () => {
+    const loader = document.getElementById("loader");
+    const showMoreButton = document.getElementById("showMoreButton");
+    if (loader && showMoreButton){
+      showMoreButton.style.display = "none";
+      loader.classList.remove("hidden");
+      loader.classList.add("flex");
+      setTimeout(() => {
+        loader.classList.remove("flex");
+        loader.classList.add("hidden");
+        showMoreButton.style.display = "flex";
+      }, 3000);
+
+    }
+    const { series: newSeries, totalPages: newTotalPages } = await fetchSeries(
       type,
       page
     );
 
     // Filtrar las películas nuevas para eliminar duplicados
-    const uniqueNewMovies = newMovies.filter(
-      (movie) => !isMovieInList(movie.id)
+    const uniqueNewSeries = newSeries.filter(
+      (serie) => !isSerieInList(serie.id)
     );
 
-    setMovies((prevMovies) => [...prevMovies, ...uniqueNewMovies]);
+    setSeries((prevSeries) => [...prevSeries, ...uniqueNewSeries]);
     setPage((prevPage) => prevPage + 1);
     setTotalPages(newTotalPages);
-
-    // Ordenar por vote_average
-    // setMovies((prevMovies) =>
-    //   prevMovies.sort((a, b) => b.vote_average - a.vote_average)
-    // );
   };
 
   const handleTypeChange = async (newType) => {
     setType(newType);
     setPage(1);
-    const { movies: newMovies, totalPages: newTotalPages } = await fetchMovies(
+    const { series: newSeries, totalPages: newTotalPages } = await fetchSeries(
       newType,
       1
     );
-    setMovies(newMovies);
+    setSeries(newSeries);
     // Ordenar por vote_average
-    setMovies((prevMovies) =>
-      prevMovies.sort((a, b) => b.vote_average - a.vote_average)
+    setSeries((prevSeries) =>
+      prevSeries.sort((a, b) => b.vote_average - a.vote_average)
     );
     setTotalPages(newTotalPages);
   };
 
-  const movieDetails = async (movieId) => {
-    window.open(`/peliculas/${movieId}`, "_self");
-  }
+  const serieDetails = async (serieId) => {
+    window.open(`/series/${serieId}`, "_self");
+  };
 
   const translateType = (type) => {
     switch (type) {
@@ -152,18 +161,17 @@ export default function MovieList({ initialMovies, initialType }) {
       case "trendingWeek":
         return "Tendencias de la Semana";
       case "nowPlaying":
-        return "En el Cine";
-      case "upcoming":
-        return "Próximamente";
+        return "En Emisión";
       default:
         return "Más Populares";
     }
   }
 
   return (
-    <div className="flex flex-col justify-center mx-6 md:mx-12 pb-20">
+    <div className="mx-12 pb-20">
       <div className="flex items-center gap-x-8 mb-12">
-        <h1 className="text-3xl font-bold text-white">Películas</h1>
+        <h1 className="text-3xl font-bold text-white">Series</h1>
+
         <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-[var(--background-color)] focus:ring-2 focus:outline-none focus:ring-[var(--primary-color)] font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center capitalize" type="button"> {translateType(type)} <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
         </svg>
@@ -174,6 +182,7 @@ export default function MovieList({ initialMovies, initialType }) {
             <ul className="py-2 text-sm text-white" aria-labelledby="dropdownDefaultButton">
               <li>
                 <button
+                type="button"
                 className="block w-full text-left px-4 py-2 hover:bg-[--primary-color-hover] hover:text-[var(--background-color)]"
                 onClick={() => handleTypeChange("top-rated")}
                           >
@@ -210,31 +219,36 @@ export default function MovieList({ initialMovies, initialType }) {
                   className="block w-full text-left px-4 py-2 hover:bg-[--primary-color-hover] hover:text-[var(--background-color)]"
                   onClick={() => handleTypeChange("nowPlaying")}
                 >
-                  En el Cine
+                  En Emisión
                 </button>
               </li>
             </ul>
         </div>
+
+        {/* <div className="text-white text-nowrap rounded-full flex items-center  gap-x-2 border-2 border-[var(--primary-color)] max-w-96 overflow-scroll xl:max-w-max">
+          
+          
+        </div> */}
       </div>
       <div className="movies-container grid grid-cols-1 gap-10 md:gap-8 md:grid-cols-4 lg:grid-cols-5 mb-24">
-        {movies.map((movie, index) => (
+        {series.map((serie, index) => (
           <div
             key={index}
             className="flex flex-col justify-start items-start gap-y-4 w-full h-full"
           >
             <img
               className="w-80 md:w-full md:h-full md:min-h-[280px] md:max-h-[280px] lg:min-h-[320px] lg:max-h-[320px] rounded-xl object-fill cursor-pointer"
-              src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+              src={`https://image.tmdb.org/t/p/original${serie.poster_path}`}
               loading="lazy"
-              onClick={() => movieDetails(movie.id)}
-              alt={movie.title}
+              onClick={() => serieDetails(serie.id)}
+              alt={serie.name}
             />
             <div className="flex flex-col gap-y-4">
               <h1 className="text-white text-2xl font-medium cursor-pointer hover:underline"
-              onClick={() => movieDetails(movie.id)}
-              >{movie.title}</h1>
+              onClick={() => serieDetails(serie.id)}
+              >{serie.name}</h1>
               <span className="text-gray-300">
-                Puntuación: {formatRating(movie.vote_average)} ({formatVotes(movie.vote_count)})
+                Puntuación: {formatRating(serie.vote_average)} ({formatVotes(serie.vote_count)})
               </span>
               {/* <div className="flex gap-x-1 py-2">
                 {movie.genres.map((genre, index) => (
@@ -253,9 +267,19 @@ export default function MovieList({ initialMovies, initialType }) {
         ))}
       </div>
       {page <= totalPages && (
-        <div className="w-full flex justify-center items-center">
+        <div  className="w-full flex justify-center items-center relative">
+          <div id="loader" class="hidden absolute flex-col justify-center items-center max-w-[300px] w-full">
+            <button disabled type="button" class="w-full py-2.5 px-5 text-2xl font-semibold text-[var(--primary-color)] bg-transparent rounded-lg   flex flex-col justify-center items-center gap-y-4">
+              <svg aria-hidden="true" role="status" class="inline size-14 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#13151a"/>
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#CBC400"/>
+              </svg>
+              Loading...
+            </button>
+          </div>
           <button
-            onClick={loadMoreMovies}
+            id="showMoreButton"
+            onClick={loadMoreSeries}
             style={{
               cursor: "pointer",
               position: "relative",
--- src/lib/tmdb.js
+++ src/lib/tmdb.js
@@ -2,9 +2,9 @@ const API_KEY = '0a233d84fc55a74ae5753ab6a5e22375';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 // Obtiene todos los géneros
-const getGenres = async () => {
+const getGenres = async (type) => {
   try {
-    const response = await fetch(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=es-ES`);
+    const response = await fetch(`${BASE_URL}/genre/${type}/list?api_key=${API_KEY}&language=es-ES`);
     if (!response.ok) {
       throw new Error('Error al obtener los géneros');
     }
@@ -16,9 +16,9 @@ const getGenres = async () => {
   }
 };
 
-const getVideos = async (movieId) => {
+const getVideos = async (movieId, type) => {
   try {
-    const response = await fetch(`${BASE_URL}/movie/${movieId}/videos?api_key=${API_KEY}&language=es-ES`);
+    const response = await fetch(`${BASE_URL}/${type}/${movieId}/videos?api_key=${API_KEY}&language=es-ES`);
     if (!response.ok) {
       throw new Error('Error al obtener los videos de la película');
     }
@@ -31,9 +31,9 @@ const getVideos = async (movieId) => {
   }
 };
 
-const getCredits = async (movieId) => {
+const getCredits = async (movieId, type) => {
   try {
-    const response = await fetch(`${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}&language=es-ES`);
+    const response = await fetch(`${BASE_URL}/${type}/${movieId}/credits?api_key=${API_KEY}&language=es-ES`);
     if (!response.ok) {
       throw new Error('Error al obtener los créditos de la película');
     }
@@ -45,12 +45,26 @@ const getCredits = async (movieId) => {
   }
 }
 
+const getProviders = async (movieId, type) => {
+  try {
+    const response = await fetch(`${BASE_URL}/${type}/${movieId}/watch/providers?api_key=${API_KEY}`);
+    if (!response.ok) {
+      throw new Error('Error al obtener los proveedores de la película');
+    }
+    const data = await response.json();
+    return data.results.ES.flatrate;
+  } catch (error) {
+    console.error('Error fetching providers:', error);
+    return [];
+  }
+}
+
 // Asocia géneros a las películas
 const associateGenresAndVideosToMovies = async (movies) => {
   try {
-    const genres = await getGenres();
+    const genres = await getGenres('movie');
     const moviesWithGenresPromises = movies.map(async (movie) => {
-      const videos = await getVideos(movie.id);
+      const videos = await getVideos(movie.id, "movie");
       return {
         ...movie,
         genres: movie.genre_ids.map(id => genres.find(genre => genre.id === id)).filter(genre => genre !== undefined),
@@ -63,6 +77,25 @@ const associateGenresAndVideosToMovies = async (movies) => {
     return [];
   }
 };
+
+// Asocia géneros a las series
+const associateGenresAndVideosToSeries = async (series) => {
+  try {
+    const genres = await getGenres('tv');
+    const seriesWithGenresPromises = series.map(async (serie) => {
+      const videos = await getVideos(serie.id, 'tv');
+      return {
+        ...serie,
+        genres: serie.genre_ids.map(id => genres.find(genre => genre.id === id)).filter(genre => genre !== undefined),
+        videos
+      };
+    });
+    return await Promise.all(seriesWithGenresPromises);
+  } catch (error) {
+    console.error('Error associating genres and videos to series:', error);
+    return [];
+  }
+}
 
 
 // Películas Populares
@@ -205,8 +238,9 @@ export const getMovieDetails = async (movieId) => {
     return {
       ...movie,
       genres: movie.genres,
-      videos: await getVideos(movieId),
-      credits: await getCredits(movieId)
+      videos: await getVideos(movieId, 'movie'),
+      credits: await getCredits(movieId, 'movie'),
+      providers: await getProviders(movieId, 'movie')
     };
   } catch (error) {
     console.error('Error fetching movie details:', error);
@@ -215,16 +249,172 @@ export const getMovieDetails = async (movieId) => {
 };
 
 // Series Populares
-export const getPopularSeries = async () => {
+export const getPopularSeries = async (page) => {
   try {
-    const response = await fetch(`${BASE_URL}/tv/popular?api_key=${API_KEY}&language=es-ES`);
+    const response = await fetch(`${BASE_URL}/tv/popular?api_key=${API_KEY}&language=es-ES&page=${page}`);
     if (!response.ok) {
       throw new Error('Error al obtener las series populares');
     }
     const data = await response.json();
-    return data.results;
+    const series = await associateGenresAndVideosToSeries(data.results);
+    return {series, totalPages: data.total_pages};
   } catch (error) {
     console.error('Error fetching popular series', error);
-    return [];
+    return {series: [], totalPages: 1};
   }
 };
+
+// Top Rated Series
+export const getTopRatedSeries = async (page) => {
+  try{
+    const response = await fetch(`${BASE_URL}/tv/top_rated?api_key=${API_KEY}&language=es-ES&page=${page}`);
+    if(!response.ok){
+      throw new Error('Error al obtener las series mejor valoradas');
+    }
+    const data = await response.json();
+    const series = await associateGenresAndVideosToSeries(data.results);
+    return { series, totalPages: data.total_pages };
+  } catch (error) {
+    console.error('Error fetching top rated movies:', error);
+    return { series: [], totalPages: 1 };
+  }
+}
+
+// Trending Series Week
+export const getTrendingSeriesWeek = async (page) => {
+  try {
+    const response = await fetch(`${BASE_URL}/trending/tv/week?api_key=${API_KEY}&language=es-ES&page=${page}`);
+    if (!response.ok) {
+      throw new Error('Error al obtener las series de tendencia');
+    }
+    const data = await response.json();
+    const series = await associateGenresAndVideosToSeries(data.results);
+    return { series, totalPages: data.total_pages };
+  } catch (error) {
+    console.error('Error fetching trending series:', error);
+    return { series: [], totalPages: 1 };
+  }
+};
+
+// Trending Series Day
+export const getTrendingSeriesDay = async (page) => {
+  try {
+    const response = await fetch(`${BASE_URL}/trending/tv/day?api_key=${API_KEY}&language=es-ES&page=${page}`);
+    if (!response.ok) {
+      throw new Error('Error al obtener las series de tendencia del día');
+    }
+    const data = await response.json();
+    const series = await associateGenresAndVideosToSeries(data.results);
+    return { series, totalPages: data.total_pages };
+  } catch (error) {
+    console.error('Error fetching trending series:', error);
+    return { series: [], totalPages: 1 };
+  }
+};
+
+// Now Playing Series
+export const getNowPlayingSeries = async (page) => {
+  try {
+    const response = await fetch(`${BASE_URL}/tv/on_the_air?api_key=${API_KEY}&language=es-ES&page=${page}`);
+    if (!response.ok) {
+      throw new Error('Error al obtener las series en emisión');
+    }
+    const data = await response.json();
+    const series = await associateGenresAndVideosToSeries(data.results);
+    return { series, totalPages: data.total_pages };
+  } catch (error) {
+    console.error('Error fetching now playing series:', error);
+    return { series: [], totalPages: 1 };
+  }
+};
+
+// Upcoming Series
+export const getUpcomingSeries = async (page) => {
+  try {
+    const response = await fetch(`${BASE_URL}/tv/airing_today?api_key=${API_KEY}&language=es-ES&page=${page}`);
+    if (!response.ok) {
+      throw new Error('Error al obtener las series próximas');
+    }
+    const data = await response.json();
+    const currentDate = new Date().toISOString().split('T')[0]; // Fecha actual en formato YYYY-MM-DD
+    const futureSeries = data.results.filter(serie => serie.first_air_date > currentDate); // Filtra series futuras
+    const totalPages = data.total_pages;
+    return {
+      series: await associateGenresAndVideosToSeries(futureSeries),
+      totalPages
+    };
+  } catch (error) {
+    console.error('Error fetching upcoming series:', error);
+    return { series: [], totalPages: 0 };
+  }
+};
+
+
+// Serie Details
+export const getSerieDetails = async (serieId) => {
+  try {
+    const response = await fetch(`${BASE_URL}/tv/${serieId}?api_key=${API_KEY}&language=es-ES`);
+    if (!response.ok) {
+      throw new Error('Error al obtener los detalles de la serie');
+    }
+    const serie = await response.json();
+    return {
+      ...serie,
+      genres: serie.genres,
+      videos: await getVideos(serieId, 'tv'),
+      credits: await getCredits(serieId, 'tv'),
+      providers: await getProviders(serieId, 'tv')
+    };
+  } catch (error) {
+    console.error('Error fetching serie details:', error);
+    return null;
+  }
+};
+
+export const getAllMoviesAndSeries = async () => {
+  try {
+    // Fetch all movies and series
+    const popularMovies = await getPopularMovies(1);
+    const trendingMoviesWeek = await getTrendingMoviesWeek(1);
+    const trendingMoviesDay = await getTrendingMoviesDay(1);
+    const nowPlayingMovies = await getNowPlayingMovies(1);
+    const topRatedMovies = await getTopRatedMovies(1);
+    const upcomingMovies = await getUpcomingMovies(1);
+    const popularSeries = await getPopularSeries(1);
+    const topRatedSeries = await getTopRatedSeries(1);
+    const trendingSeriesWeek = await getTrendingSeriesWeek(1);
+    const trendingSeriesDay = await getTrendingSeriesDay(1);
+    const nowPlayingSeries = await getNowPlayingSeries(1);
+    const upcomingSeries = await getUpcomingSeries(1);
+
+
+    // Save all items in an items[] array
+    const items = [
+      ...popularMovies.movies,
+      ...trendingMoviesWeek.movies,
+      ...trendingMoviesDay.movies,
+      ...nowPlayingMovies.movies,
+      ...topRatedMovies.movies,
+      ...upcomingMovies.movies,
+      ...popularSeries.series,
+      ...topRatedSeries.series,
+      ...trendingSeriesWeek.series,
+      ...trendingSeriesDay.series,
+      ...nowPlayingSeries.series,
+      ...upcomingSeries.series
+    ];
+
+    // Delete repeated items
+    const uniqueItems = items.filter((item, index, self) =>
+      index === self.findIndex((t) => (
+        t.id === item.id
+      ))
+    );
+    return uniqueItems;
+
+
+  } catch (error) {
+    console.error('Error fetching all movies and series:', error);
+    return [];
+  }
+}
